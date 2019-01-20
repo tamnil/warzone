a4d72562e73c4f95e9a696317071f3352cab4859
Tamnil Saito Junior <tamnil@gmail.com>
2019-01-20 19:29:31 -0200

    tests added

---
+++ README.md
@@ -0,0 +1,13 @@
+# Template for tests and development
+
+
+
+## Included in package.json:
+
+* "gulp": "^4.0.0",
+* "jshint": "2.9.7",
+* "npm-watch": "^0.5.0",
+* "mocha": "^5.2.0",
+* "chai": "^4.2.0",
+* "sinon": "^7.2.2"
+
---
+++ test/warzone.test.js
@@ -0,0 +1,65 @@
+/*
+*       WARZONE Test template
+*
+*
+*/
+
+
+const assert = require('assert');
+const should = require('chai').should;
+
+// mission control
+
+//setup flags
+const URL = '';
+
+
+describe('tests template',() => {
+    describe('mission1 - simulation',() => {
+
+        it('',() => {
+
+        })
+
+    })
+
+})
+
+describe('accepted tasks',() => {
+    describe('',() => {
+
+        it('',() => {
+
+        })
+
+    })
+
+})
+
+describe('tests template',() => {
+    describe('mission1 - simulation',() => {
+
+        it('',() => {
+
+        })
+
+    })
+
+})
+
+/*
+describe('rejected tasks',() => {
+
+    describe('its not too bad',() => {
+
+        it('',() => {
+
+        })
+
+    })
+
+})
+
+/*
+
+*/
